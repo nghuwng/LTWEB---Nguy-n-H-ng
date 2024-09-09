78021eda6c47580a81f0d9ec7366f9cbdd4827b6
nghuwng <86052583+nghuwng@users.noreply.github.com>
2024-09-09 22:50:18 +0700

    Add files via upload
    first commit

---
+++ HelloWorld/src/main/java/hungvt/DBConnectionSQLServer.java
@@ -0,0 +1,12 @@
+package hungvt;
+
+public class DBConnectionSQLServer {
+	 private final String serverName = "localhost";
+	 private final String dbName = "";
+	 private final String portNumber = "1433";
+	 private final String instance = "";
+	 private final String userID= "";
+	 private final String password= "";
+	 
+
+}
---
+++ HelloWorld/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Thông tin cá nhân</title>
+</head>
+<body>
+    <h1>Thông tin cá nhân</h1>
+    <p>Họ và tên: Trần Nguyên Hưng</p>
+    <p>Mssv: 20146057</p>
+    <p>Email: 20146057@student.hcmute.edu.vn</p>
+</body>
+</html>
